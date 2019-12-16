d9de6a9d8002549544ed8e3459869ace04a1f311
mohammadhasan <47121464+mohammadhasananisi@users.noreply.github.com>
2019-12-16 15:24:37 +0330

    Create index.php
    This file to change the technology of your site

---
+++ index.php
@@ -0,0 +1,164 @@
+<?php
+//cleverboyqom.ir
+session_name("laravel_session");
+session_start();
+$_SESSION['laravel_session'] = '60496c0795af2c5a1851100f52f34ccf3c692279+SFqbxMoR1OsEMJsYnZ11ANbTYFp8HmihXoDEaD6O';
+//go 
+setcookie('REVEL_FLASH','Path=/; HttpOnly');
+// Express
+header("X-Powered-By: Express");
+// cowboy
+header("server: Cowboy");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!-- Add Python,PHP, Perl, Django, Drupal, TYPO3, Joomla, MediaWiki, MODX, PrestaShop, SPIP, Jekyll, React, Hexo, Gatsby, CakePHP, WordPress, phpmyadmin,ruby, nodejs, yii, Laravel, IIS and ASP.NET to site technology -->
+<!-- for MediaWiki -->
+<meta name="generator" content="MediaWiki 1.35.0-anisi.5"/>
+<meta name="generator" content="Bolt v232323"/>
+<!-- end -->
+<!-- for Drupal -->
+<meta name="generator" content="Drupal 1600000.9.10 (https://www.drupal.org)" />
+<!-- end -->
+<!-- for joomla -->
+<meta name="generator" content="Joomla! 1600000.9.10 - Open Source Content Management" />
+<!-- end -->
+<!-- for typo3 -->
+<meta name="generator" content="TYPO3 CMS 1600000.9.10" />
+<!-- end -->
+<!-- for modx -->
+<meta name="generator" content="MODX  1600000.9.10" />
+<!-- end -->
+<!-- for PrestaShop cms -->
+<meta name="generator" content="PrestaShop CMS" />
+<!-- end -->
+<!-- for SPIP -->
+<meta name="generator" content="SPIP 1600000.9.10" />
+<!-- end -->
+<!-- for Jekyll -->
+<meta name="generator" content="Jekyll v1600000.9.10">
+<!-- end -->
+<!-- for React -->
+<meta name="generator" content="React v1600000.9.10">
+<!-- end -->
+<!-- for hexo -->
+<meta name="generator" content="Hexo 1600000.9.10">
+
+<!-- end -->
+<!-- for Gatsby -->
+<meta name="generator" content="Gatsby 1600000.9.10"/>
+<!-- end -->
+<!-- for framework  cake php -->
+<meta name="apple-mobile-web-app-title" content="CakePHP">
+  <meta name="application-name" content="CakePHP">
+ <!-- end -->
+<!-- for wordpress-->
+
+<meta name="generator" content="WordPress 1600000.9.10" />
+ <!-- end  -->
+<!-- for phpmyadmin  -->
+<link rel="stylesheet" type="text/css" href="phpmyadmin.css.php?lang=en-iso-8859-1&amp;convcharset=iso-8859-1&amp;collation_connection=utf8_unicode_ci&amp;token=355be9a934a5537fd5a7dea6b9145a06&amp;js_frame=right&amp;nocache=4099247312" />
+<!-- end  -->
+
+<!-- for ruby -->
+<meta name="csrf-param" content="authenticity_token">
+    <meta name="csrf-token" content="bfKvuTkAv0T0kaxzH2vjxViEgkwYvtuKhRjIJY0PP2kQaP8rWQOKTjzLzxdR1OB/uFuWG6VUBicjuNbUnNppqQ==">
+<!-- end -->
+    <!-- for nodejs -->
+    <script charset="utf-8" src="https://cdn-client.medium.com/lite/static/js/tracing.075b133f.chunk.js"></script>
+    <!-- end -->
+
+<!-- for Asciidoc cms -->
+    <meta name="generator" content="AsciiDoc 17.10.1383" />
+    <!-- end -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- for Bolt -->
+  <meta name="generator" content="Bolt 1600000.9.10" />
+  <!-- end -->
+  <title>Document</title>
+</head>
+<body>
+  
+
+<!-- Put the code in <head> tag -->
+
+<!-- If you want to change the version, replace your version with 7 -->
+
+
+<!-- for yii framework php -->
+<div style="display: none;">
+<form id="login-form" action="https://app.qr-code-generator.com/site/login" method="post">
+<div style="display:none"><input type="hidden" value="f83cd92622509ebf18cd112c08dad69788f0b491" name="YII_CSRF_TOKEN" /></div>  <div class="">
+    <input placeholder="E-Mail" name="LoginForm[email]" id="LoginForm_email" type="text" />    <div class="errorMessage" id="LoginForm_email_em_" style="display:none"></div>  </div>
+  <div class="">
+
+    <input placeholder="Password" name="LoginForm[password]" id="LoginForm_password" type="password" />    <div class="errorMessage" id="LoginForm_password_em_" style="display:none"></div>  </div>
+  <div class="rememberMe clr box-footer">
+    <div class="checkbox" style="margin-left: -30px !important;">
+            <span class="checkboxes-container green-checkbox">
+        <input id="ytLoginForm_rememberMe" type="hidden" value="0" name="LoginForm[rememberMe]" /><input class="activated" name="LoginForm[rememberMe]" id="LoginForm_rememberMe" value="1" type="checkbox" />                <label for="LoginForm_rememberMe">Remember me</label>
+            </span>
+        </div>
+        <div class="errorMessage" id="LoginForm_rememberMe_em_" style="display:none"></div>
+    <input class="btn btn-primary btn-lg" type="submit" name="yt0" value="Log In" />
+    <div class="text-center">
+
+  </div>
+  </form>
+</div>
+ <!--end  -->
+
+
+
+
+     
+    <!-- for perl -->
+    <div style="display: none;"><!-- /cgi-bin/koha/opac-user.pl --><!-- /cgi-bin/koha/opac-user.pl --><form action="/cgi-bin/koha/opac-user.pl" method="post" name="auth" id="modalAuth"><div class="modal-body"><input type="hidden" name="koha_login_context" value="opac" /><fieldset class="brief">
+                        <label for="muserid">username :</label><input type="text" id="muserid" name="userid" />
+                        <label for="mpassword">password:</label><input type="password" id="mpassword" name="password" />
+                        <div id="forgotpassword-modal"><a href="/cgi-bin/koha/opac-password-recovery.pl"></a></div></fieldset></div><div class="modal-footer"><input type="submit" value="login" class="btn btn-primary" /></div></form> </div>
+<!-- end -->
+
+<!-- for python -->
+
+    <div id="content-main" style="display: none;">
+<form action="/admin/login/?next=/admin/" method="post" id="login-form"><input type="hidden" name="csrfmiddlewaretoken" value="dhLET8KO9DYpu29yAa8NRaOgjCXii0rZvyj7hZg5mWT50b1ojrwJNLAJMDQmLsaq">
+  <div class="form-row">
+    <label class="required" for="id_username">Username:</label> <input type="text" name="username" autofocus autocapitalize="none" autocomplete="username" maxlength="150" required id="id_username">
+  </div>
+  <div class="form-row">
+    <label class="required" for="id_password">Password:</label> <input type="password" name="password" autocomplete="current-password" required id="id_password">
+    <input type="hidden" name="next" value="/admin/">
+  </div>
+  <div class="submit-row">
+    <label>&nbsp;</label><input type="submit" value="Log in">
+  </div>
+</form>
+</div> 
+<!-- end -->
+
+
+<!-- for laravel -->
+<div style="display: none;">
+use on php tage
+session_name("laravel_session");
+session_start();
+$_SESSION['laravel_session'] = '60496c0795af2c5a1851100f52f34ccf3c692279+SFqbxMoR1OsEMJsYnZ11ANbTYFp8HmihXoDEaD6O';
+</div>
+<!-- end -->
+
+<!-- Windows Server IIS, asp.net -->
+<div class="aspNetHidden" style="display: none;">
+	<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="71891709" />
+	<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="/wEdAAUlrobvPz2cv7jIIZx6SdWEyvLAEmW17KJc/0whW4xPZO7RLADZm46XRP45LfnJejD6ac5/4MVxG4SMkdGj8uGNZHwODS+/32/3Tj1bRSppsZS51KSKg0GdEovPDpyG1bg+2StZ" />
+</div>
+<!-- Dart And Angulardart -->
+<script type="text/javascript" src="/js/ext-main/ext-all.js"></script>
+  <script type="text/javascript" src="/js/dart/build/ds-all-min.js"></script>
+  <!-- end -->
+</body>
+</html>
